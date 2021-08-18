8ae0cf9132e370edd354be99fc87483a109da656
Sujay Pillai <sujayopillai@gmail.com>
2021-08-18 12:56:03 -0400

    Updated with dagger todoapp
    Signed-off-by: Sujay Pillai <sujayopillai@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
